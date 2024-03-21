0380bc3e8b6dfe4f305c045df5eec5a75854878b
SeanF <65409216+Purpletetle@users.noreply.github.com>
2024-03-20 21:51:34 -0400

    ceaser.py

--- ceaser.py
+++ ceaser.py
@@ -4,15 +4,9 @@ key = int(input("Enter the key: "))
 
 def caesarsCipher(plainText, key):
     cipherText = ""
-    for char in plainText:
-        # Let's check if the character is a letter
+    for char in plainText
         if char.isalpha():
-            # Let's check if the character is upper or lower case
             if char.islower():
-                # chr returns the character that corresponds to the ASCII code
-                # ord returns the ASCII code that corresponds to the character
-                # We use the ASCII code to shift the character by the key
-                # We use the modulo operator to wrap around the alphabet
                 cipherText += chr((ord(char) + key - 97) % 26 + 97)
             else:
                 cipherText += chr((ord(char) + key - 65) % 26 + 65)
